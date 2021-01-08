ade5cf6b9d499b21bdd807922adab791a17eff2c
Hyeonhee Jang <wkdgus7113@gmail.com>
2021-01-08 18:26:50 +0900

    4673 Done

--- Project1/4673 셀프넘버.c
+++ Project1/4673 셀프넘버.c
@@ -1,15 +1,31 @@
 #include<stdio.h>
 
-void d(number) {
+void printSelfNumber() {
+	int i, sum, n;
+	int arr[10001] = { 0 };
 
+	for (i = 1; i <= 10000; i++) {
+		sum = i, n = i;
 
-	
+		while (1) {
+			// exit: 더할 값이 0일 경우 (모든 자리의 숫자를 더함)
+			if (n == 0) break;
+
+			sum += n % 10;	// 현재의 1의자리 더하기
+			n /= 10;		// 현재의 1의자리 없애기
+		}
+
+		// index overflow 방지하면서 arr 변경
+		if(sum <= 10000) arr[sum]++;
+	}
+
+	// 출력
+	for (i = 1; i < 10000; i++) {
+		if (arr[i] == 0) printf("%d\n", i);
+	}
 }
 
 int main(void) {
-	int i;
-	for (i = 1; i < 10000; i++) {
-		d(i);
-	}
+	printSelfNumber();
 	return 0;
 }
