18e761f6fd0238060530ca58b5747bef07050005
huey J <wkdgus7113@gmail.com>
2021-02-21 19:45:30 +0900

    Add 2 solutions for 프로그래머스 Lv1 두 개 뽑아서 더하기 크레인 인형뽑기 게임

---
+++ 파이썬/프로그래머스 Lv1/두 개 뽑아서 더하기 (완전탐색).py
@@ -0,0 +1,13 @@
+def solution(numbers):
+    sum = []
+    for i in range(0, len(numbers)-1):
+        for j in range(i+1, len(numbers)):
+            sum.append(numbers[i] + numbers[j])
+
+    answer = list(set(sum))
+    answer.sort()
+    return answer
+
+
+inputs = list(map(int, input().split()))
+print(solution(inputs))
