c2b95d5ee770ab422071b48eeff16b22a1b722e9
huey J <wkdgus7113@gmail.com>
2021-02-22 12:35:48 +0900

    Add 2 solutions for 프로그래머스 Lv1 k번째 수 체육복

---
+++ 파이썬/프로그래머스 Lv1/체육복 (탐욕법).py
@@ -0,0 +1,72 @@
+n = 3
+lost = [3]
+reserve = [1]
+
+students = [1] * n
+for i in lost:
+    students[i - 1] -= 1
+for i in reserve:
+    students[i - 1] += 1
+
+answer = 0
+for i in range(len(students)):
+    if students[i] == 0:  # 잃어버린 학생일 경우
+        if i == 0:  # 잃어버린 학생이 맨 처음
+            if students[i + 1] == 2:
+                students[i] = 1
+                students[i + 1] = 1
+                answer += 1
+        elif i == len(students) - 1:  # 잃어버린 학생이 마지막
+            if students[i - 1] == 2:
+                students[i] = 1
+                students[i - 1] = 1
+                answer += 1
+        else:  # 잃어버린 학생이 처음 또는 마지막이 아님
+            if students[i - 1] == 2:
+                students[i] = 1
+                students[i - 1] = 1
+                answer += 1
+            elif students[i + 1] == 2:
+                students[i] = 1
+                students[i + 1] = 1
+                answer += 1
+    else:
+        answer += 1
+
+print(answer)
+
+
+#### 내 답안
+# def solution(n, lost, reserve):
+#     students = [1] * n
+#     for i in lost:
+#         students[i - 1] -= 1
+#     for i in reserve:
+#         students[i - 1] += 1
+#
+#     answer = 0
+#     for i in range(len(students)):
+#         if students[i] == 0:  # 잃어버린 학생일 경우
+#             if i == 0:  # 잃어버린 학생이 맨 처음
+#                 if students[i + 1] == 2:
+#                     students[i] = 1
+#                     students[i + 1] = 1
+#                     answer += 1
+#             elif i == len(students) - 1:  # 잃어버린 학생이 마지막
+#                 if students[i - 1] == 2:
+#                     students[i] = 1
+#                     students[i - 1] = 1
+#                     answer += 1
+#             else:  # 잃어버린 학생이 처음 또는 마지막이 아님
+#                 if students[i - 1] == 2:
+#                     students[i] = 1
+#                     students[i - 1] = 1
+#                     answer += 1
+#                 elif students[i + 1] == 2:
+#                     students[i] = 1
+#                     students[i + 1] = 1
+#                     answer += 1
+#         else:
+#             answer += 1
+#
+#     return answer
